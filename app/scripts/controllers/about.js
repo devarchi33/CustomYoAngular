f9c6bbdcd8fd8821a29bd89770ed6cfb19c5dcea
skyfly33 <skyfly33727@gmail.com>
2015-07-27 14:29:57 +0900

    angular $http.get example add.

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -7,23 +7,29 @@
  * # AboutCtrl
  * Controller of the skyfly33App
  */
-angular.module('skyfly33App')
-  .controller('AboutCtrl', function ($scope) {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+ angular.module('skyfly33App')
+ .controller('AboutCtrl', function ($scope, $http) {
+ 	this.awesomeThings = [
+ 	'HTML5 Boilerplate',
+ 	'AngularJS',
+ 	'Karma'
+ 	];
 
-    $scope.population = 700000000;
-    $scope.countries = [
-    	{
-    		name : 'korea',
-    		location : 'Asia' 
-    	},
-    	{
-    		name : 'USA',
-    		location : 'America'
-    	}
-    ];
-  });
+ 	$scope.population = 700000000;
+ 	$scope.countries = [
+ 	{
+ 		name : 'korea',
+ 		location : 'Asia' 
+ 	},
+ 	{
+ 		name : 'USA',
+ 		location : 'America'
+ 	}
+ 	];
+
+ 	$http.get("http://www.w3schools.com/angular/customers.php")
+ 	.success(
+ 		function(response){
+ 			$scope.names = response.records;
+ 	});
+ });
